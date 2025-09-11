639a098abf128c579c44ca1352f90799220cafc3
soheib5566 <soheib5566@gmail.com>
2025-09-11 22:46:52 +0300

    fix : add HasFactory

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,12 +5,13 @@ namespace App\Models;
 use App\Models\Category;
 use App\Models\OrderItem;
 use App\Models\ProductLog;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, HasFactory;
 
     protected $fillable = ['name', 'description', 'price', 'stock', 'image', 'category_id'];
 
